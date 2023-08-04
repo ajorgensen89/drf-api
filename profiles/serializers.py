7379c918b46e2d9d69f836f0e66cf041178daa08
ajorgensen89 <ajorgensen89@gmail.com>
2023-08-04 14:30:50 +0000

    start CRUD table

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,12 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    profile_name = serializers.ReadOnlyField(source='owner.profile.name')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'content',
-            'image'
+            'image', 'profile_name', 'profile_image'
         ]

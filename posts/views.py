ffff4555fc6048ca5f112f7d1c46587c9a2f0b99
ajorgensen89 <ajorgensen89@gmail.com>
2023-08-09 12:42:50 +0000

    retrieve edit and delete post

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from django.http import Http404
 
 
 class PostList(APIView):
@@ -23,7 +24,7 @@ class PostList(APIView):
             posts, many=True, context={'request': request}
         )
         return Response(serializer.data)
-    
+
     # Check info coming it and throw error or valid status
     def post(self, request):
         serializer = PostSerializer(
@@ -36,4 +37,49 @@ class PostList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PostDetail(APIView):
+    # permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    # get posts
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    # update posts
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # delete post
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )

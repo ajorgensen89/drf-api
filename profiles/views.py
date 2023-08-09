cdd2cf22a74df1e5be56b9c4668b43025ddb24eb
ajorgensen89 <ajorgensen89@gmail.com>
2023-08-09 10:44:52 +0000

    Profile detail GET/PUT

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,5 +10,37 @@ from .serializers import ProfileSerializer
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
+        # To get many profiles
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    # self and primary key as arguments.
+    
+    def get_object(self, pk):
+        try:
+            # Get profile by primary key
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            # import at the top
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        # To get one profile
+        serializer = ProfileSerializer(profile)
+        # return data in a Response
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
